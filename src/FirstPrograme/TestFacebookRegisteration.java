c6909c96ab7785419b1da383f927ba65cb3368fd
Sumit.Sahay <smt.sahay@gmail.com>
2016-10-18 18:23:12 +0100

    Testcase update

--- src/FirstPrograme/TestFacebookRegisteration.java
+++ src/FirstPrograme/TestFacebookRegisteration.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.ui.Select;
 public class TestFacebookRegisteration 
 
 {
-	public static void main (String [] args)
+	public static void main (String [] args) throws InterruptedException
 	
 	{
 		System.setProperty("webdriver.gecko.driver","C:\\Selenium\\geckodriver.exe");
@@ -21,24 +21,26 @@ public class TestFacebookRegisteration
 		driver.findElement(By.xpath(".//*[@id='u_0_1']")).sendKeys("Sumit");
 		driver.findElement(By.xpath(".//*[@id='u_0_3']")).sendKeys("Sahay");
 		driver.findElement(By.xpath(".//*[@id='u_0_e']")).click();
+		Thread.sleep(3000);
 		
 		// for selecting in dropdown we use select method for that we need to create an object
 		
 		Select sel1 = new Select(driver.findElement(By.xpath(".//*[@id='day']")));
 		sel1.selectByIndex(0);
-		
+		Thread.sleep(3000);
 		Select sel2 = new Select(driver.findElement(By.xpath(".//*[@id='month']")));
 		sel2.selectByVisibleText("Feb");
-		
+		Thread.sleep(3000);
 		Select sel3 = new Select(driver.findElement(By.xpath(".//*[@id='year']")));
 		sel3.selectByValue("2013");
-		
+		Thread.sleep(3000);
 		driver.findElement(By.xpath(".//*[@id='u_0_i']")).click();
-		
+		Thread.sleep(3000);
 		// clicking on link
 		
 		driver.findElement(By.xpath(".//*[@id='reg_pages_msg']/a")).click();
-		driver.navigate().back();
+		Thread.sleep(3000);
+		//driver.navigate().back();
 		//driver.quit();
 		
 	}
